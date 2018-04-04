64e6555454b3286c82f8aa37032d1d34ca259a44
Salvarajah, Jheeven <jsalvarajah@lcs-mba3020.lcs.on.ca>
2018-04-04 10:37:03 -0400

    Working version

--- HappyOrSad/main.swift
+++ HappyOrSad/main.swift
@@ -11,7 +11,50 @@ import Foundation
 // Get the user input
 var rawInput = readLine()
 
+// Make sure input was given (create a string from the string)
+guard let input = rawInput else {
+    //Error
+    exit(9)
+}
+
 // Print out the input provided
 print("You said:")
-print(rawInput)
+print(rawInput) // Optional String
+print(input) //Non-Optional
+
+var happyFace = 0
+var sadFace = 0
+// process- inspect each charachter
+for individualCharacter in input {
+    //Debug
+    print(individualCharacter)
+    
+    // Catogroize each character
+    if individualCharacter == "😃" {
+        // Track a Happy
+        happyFace += 1
+    } else if individualCharacter == "☹️" {
+        //track a sad
+        sadFace += 1
+    } else if individualCharacter == "😀" {
+        // Track happy
+        happyFace += 1
+    } else if individualCharacter == "😕" {
+        //Track a sad
+        sadFace += 1
+    }
+    
+}
+
+if happyFace > sadFace {
+    print("Happy")
+} else if sadFace > happyFace {
+    print("Sad")
+} else {
+    print("Unsure")
+}
+
+
+//Output
+// Tell the user "happy", "sad" or "unsure" based on counts above
 
